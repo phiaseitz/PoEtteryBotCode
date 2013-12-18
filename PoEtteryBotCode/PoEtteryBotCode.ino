587c361ade65ddd108057acc49b1f3a213d4a6ac
Sophia Seitz <sophia.seitz@students.olin.edu>
2013-12-18 11:25:54 -0500

    Better running code!

--- PoEtteryBotCode/PoEtteryBotCode.ino
+++ PoEtteryBotCode/PoEtteryBotCode.ino
@@ -2,7 +2,7 @@
 
 #include <AccelStepper.h>
 //All the Motor Things
-int motorSpeed = 1000; //maximum steps per second (about 3rps / at 16 microsteps)
+int motorSpeed = 3000; //maximum steps per second (about 3rps / at 16 microsteps)
 //int motorAccel = 80000; //steps/second/second to accelerate
 
 int motorDirPin = 2; //digital pin 2
@@ -38,8 +38,9 @@ const int maxPressure = 1000;
 //Positive (Down, Left)
 
 const float deltaPosBreak = 0.111/1;
-const float targetPoints[2][2] = {{-1,0},{-1,1}};
+const float targetPoints[4][2] = {{0,-1},{1.15, -1},{3,0}, {0,0}};
 int targetIndex = 0;
+int finished = 0;
 float target[2] = {targetPoints[targetIndex][0], targetPoints[targetIndex][1]};
 float pos[2] = {0,0};
 
@@ -49,12 +50,12 @@ float pos[2] = {0,0};
 void setup(){
   Serial.begin(9600); 
   //Motors
-  stepper1.setMaxSpeed(motorSpeed);
-  stepper1.setSpeed(motorSpeed);
+  stepper1.setMaxSpeed(5000);
+  stepper1.setSpeed(0);
   
   //Negative Speed is Up
-  stepper2.setMaxSpeed(motorSpeed);
-  stepper2.setSpeed(-motorSpeed);
+  stepper2.setMaxSpeed(5000);
+  stepper2.setSpeed(0);
   
   //Break Beam Sensors
   //horizontal
@@ -69,14 +70,15 @@ void loop(){
   int vBreakBeamReading = digitalRead(verticalBreakBeamPin);
   int pressure = 5;
   if (hBreakBeamReading == 1 && lastHBreakBeam == 0) {
+    //Serial.println("h");
     if (delta[0] > 0) {   
       pos[0] = pos[0] +  deltaPosBreak;
     } else {
       pos[0] = pos[0] -  deltaPosBreak;
     }
-    Serial.println("Horizontal");
   }
   if (vBreakBeamReading == 1 && lastVBreakBeam == 0) {
+    //Serial.println("v");
     if (delta[1] > 0) {   
       pos[1] = pos[1] +  deltaPosBreak;
     } else {
@@ -84,24 +86,33 @@ void loop(){
     }
   }
   //Are we close enough to the target point
-  if (abs(delta[0]) <= deltaPosBreak && abs(delta[1]) <= 2*deltaPosBreak)  {
+  if (abs(delta[0]) <= 2*deltaPosBreak && abs(delta[1]) <= 2*deltaPosBreak)  {
+    //Serial.println("t");
     //Increment the target point by one
     targetIndex = targetIndex+1;
+    if (targetIndex == 5) {
+      finished = 1;
+    }
     //Update our new target position
     target[0] = targetPoints[targetIndex][0];
     target[1] = targetPoints[targetIndex][1];
-    Serial.print("Moving on to target position: ");
-    Serial.print(target[0]);
-    Serial.print(",");
-    Serial.println(target[1]);
+
 //We aren't there yet
 }else{
-  if (pressure < maxPressure) {
+  if ((pressure < maxPressure) && (finished == 0)) {
     int hMotorSpeed = delta[0] * motorSpeed;
     int vMotorSpeed = delta[1] * motorSpeed;
     
+    if (abs(delta[0]) < 2*deltaPosBreak) {
+      hMotorSpeed = 0;
+    }
+    
+    if (abs(delta[1]) < 2*deltaPosBreak) {
+      vMotorSpeed = 0;
+    }
    stepper1.setSpeed(hMotorSpeed);
    stepper2.setSpeed(-vMotorSpeed);
+   
   } else{
     stepper1.setSpeed(0);
     stepper2.setSpeed(0);
@@ -114,3 +125,4 @@ void loop(){
   lastHBreakBeam = hBreakBeamReading;
   lastVBreakBeam = vBreakBeamReading;
 }
+
